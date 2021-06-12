1d496f179c55e57c60027fb72e1ea93e6a0063aa
Alexander Toro <ratoro0199@gmail.com>
2021-06-12 03:02:31 -0400

    changed the url endpoints to be part of config file

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -4,10 +4,9 @@ const fetch = require("node-fetch");
 const request = require("request");
 const config = require("../config.json");
 
+let url = config["url-endpoint"];
 router.post("/promisePost", function (req, res, next) {
-  fetch(
-    "https://api.darksky.net/forecast/dc0acce184a9972f7feab226cd4ae092/37.8267,-122.4233"
-  )
+  fetch(url)
     .then((res) => {
       return res.json();
     })
@@ -17,24 +16,19 @@ router.post("/promisePost", function (req, res, next) {
 });
 
 router.post("/asyncPost", async function (req, res, next) {
-  const response = await fetch(
-    "https://api.darksky.net/forecast/dc0acce184a9972f7feab226cd4ae092/37.8267,-122.4233"
-  );
+  const response = await fetch(url);
   const data = await response.json();
   console.log(data);
 });
 
 router.post("/callbackPost", function (req, res) {
-  request(
-    "https://api.darksky.net/forecast/dc0acce184a9972f7feab226cd4ae092/37.8267,-122.4233",
-    function (error, response, body) {
-      if (error) {
-        console.error("error:", error);
-      }
-      const newbody = JSON.parse(body);
-      console.log(body.json());
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error("error:", error);
     }
-  );
+    const newbody = JSON.parse(body);
+    console.log(body.json());
+  });
 });
 
 module.exports = router;
